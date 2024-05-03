5fcf6011f79f77ade543f158a13431295361554e
Danna Velásquez <dannamvb95@gmail.com>
2024-05-02 20:50:37 -0500

    Home and header components are added

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import './header.styles.css'
+import DoneAllOutlinedIcon from '@mui/icons-material/DoneAllOutlined';
+
+function Header() {
+  return (
+    <>
+      <div className="header-container">
+        <h1 className='title'>
+        SummarQuick 
+        <DoneAllOutlinedIcon fontSize='large' style={{marginTop: '8px', marginLeft:'5px', position:'absolute'}}/>
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Header;
--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -5,9 +5,9 @@ import "./home.styles.css";
 
 function Home() {
   return (
-    <>
+    <div className="cover">
       <div className="container">
-        <h1>
+        <h1 style={{fontSize:'50px'}}>
           Welcome to Article Summarizer IA
           <DescriptionOutlinedIcon fontSize="large" />
         </h1>
@@ -15,10 +15,11 @@ function Home() {
           id="outlined-basic"
           label="Submit your article URL here"
           variant="outlined"
+          style={{fontWeight:'bold', color:'blue'}}
         />
         <Button variant="contained">RESUME NOW</Button>
       </div>
-    </>
+    </div>
   );
 }
 
